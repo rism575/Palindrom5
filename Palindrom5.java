d04b9312dc73614e09cb7b7413d8756fe58dae65
rism575 <132285072+rism575@users.noreply.github.com>
2023-10-22 21:11:05 +0700

    Create Palindrom5.java

---
+++ Palindrom5.java
@@ -0,0 +1,21 @@
+import java.until.Scanner;
+
+class Main{
+  public static void main(String args[]){
+    String = normal, dibalik = "";
+    Scanner input= new Scanner (System.in);
+    System.out.println("input:");
+    normal = input.nextline();
+    
+    int length = normal.lenght();
+    for(int x = lenght - 1; x >=0 ;x--){
+      dibalik = dibalik + normal.charAt(x);
+      }
+      
+      if(normal.equals(dibalik)){
+        System.out.println("inputan adalah palindrom");
+        }else{
+        System.out.println("inputan adalah bukan palndrom");
+     } 
+  } 
+}
